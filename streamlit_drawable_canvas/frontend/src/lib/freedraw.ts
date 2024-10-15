2dcb8709326630521d4ad9ef246fc56f49f2c89c
jackhhao <36602317+jackhhao@users.noreply.github.com>
2024-10-15 17:24:45 -0400

    set freedraw brush (fix fabricjs 6.0 compatibility)

--- streamlit_drawable_canvas/frontend/src/lib/freedraw.ts
+++ streamlit_drawable_canvas/frontend/src/lib/freedraw.ts
@@ -1,4 +1,5 @@
 import FabricTool, { ConfigureCanvasProps } from "./fabrictool"
+import { PencilBrush } from "fabric"
 
 class FreedrawTool extends FabricTool {
   configureCanvas({
@@ -6,10 +7,20 @@ class FreedrawTool extends FabricTool {
     strokeColor,
   }: ConfigureCanvasProps): () => void {
     this._canvas.isDrawingMode = true
-    if (this._canvas.freeDrawingBrush) {
-      this._canvas.freeDrawingBrush.width = strokeWidth
-      this._canvas.freeDrawingBrush.color = strokeColor
-    }
+    this._canvas.freeDrawingBrush = new PencilBrush(this._canvas)
+    // if (this._canvas.freeDrawingBrush) {
+    //   // console.log("Free drawing brush exists");
+    this._canvas.freeDrawingBrush.width = strokeWidth
+    this._canvas.freeDrawingBrush.color = strokeColor
+
+    // TODO: add handler to convert from bezier path to line coords
+    // this._canvas.on("path:created", (e: any) => {
+    //     // console.log("Path created");
+    //     // console.log(e);
+    //     // console.log(this._canvas.toJSON());
+    //     // this.saveState(this._canvas.toJSON())
+    // })
+
     return () => {}
   }
 }
